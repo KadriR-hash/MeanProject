1d886377340e56e2f6facf9bb4351483a7ecb88a
KadriR-hash <kadriraed11@gmail.com>
2022-06-11 13:07:41 +0100

    plats

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 //API
+
 //import express
 const express = require('express');
 
@@ -9,9 +10,15 @@ const mongoose = require('mongoose');
 // import model User
 const User = require('./models/user');
 
+// import model Plat
+const Plat = require('./models/plat');
+
 //import body-parser
 const bodyParser = require('body-parser');
+const user = require('./models/user');
 
+// import bcrypt
+const bcrypt = require("bcrypt");
 
 //create express app
 const app = express();
@@ -44,14 +51,17 @@ mongoose.connect('mongodb://localhost:27017/Mean', { useNewUrlParser: true, useU
 // Business Logic : Add User
 app.post("/users", (req, res) => {
     console.log("HERE IN ADD USER");
+
+    bcrypt.hash(req.body.password, 10).then(cryptedPassword=>{
     //collect data from req body
     let user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: cryptedPassword,
         tel: req.body.tel,
-        role : req.body.role    })
+        role: req.body.role
+    })
 
     //save 
     user.save();
@@ -60,37 +70,38 @@ app.post("/users", (req, res) => {
     res.status(200).json({
         message: "USER ADDED WITH SUCCESS"
     })
+    });
 
 });
 
 // Business Logic :Get All User
-app.get("/users",(req,res)=>{
+app.get("/users", (req, res) => {
 
     console.log("HERE IN GET ALL USERS");
 
-    User.find((err,docs)=>{
-            
+    User.find((err, docs) => {
+
         if (err) {
             console.log("ERROR IN DB");
         } else {
             res.status(200).json({
-                users : docs
+                users: docs
             })
         }
     })
 })
 // Business Logic :Get One User by id
-app.get("/users/:id",(req,res)=>{
+app.get("/users/:id", (req, res) => {
 
     console.log("HERE IS GET USER BY ID");
     let userId = req.params.id;
-    User.findOne({_id : userId}).then(
-        (doc)=>{
+    User.findOne({ _id: userId }).then(
+        (doc) => {
             if (!doc) {
                 console.log("ERROR");
             } else {
                 res.status(200).json({
-                    user : doc
+                    user: doc
                 })
             }
         }
@@ -99,50 +110,157 @@ app.get("/users/:id",(req,res)=>{
     )
 })
 // Business Logic :update user
-app.put("/users/:id",(req,res)=>{
-
+app.put("/users/:id", (req, res) => {
     console.log("HERE IN UPDATE USER");
+    bcrypt.hash(req.body.password, 10).then(cryptedPassword =>{
+    let user = {
+        _id: req.body._id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: cryptedPassword,
+        tel: req.body.tel,
+        role: req.body.role
 
-    let user ={
-        _id : req.body._id,
-        firstName : req.body.firstName,
-        lastName : req.body.lastName,
-        email : req.body.email,
-        password : req.body.password,
-        tel : req.body.tel,
-        role : req.body.role,
-        
     }
 
-    User.updateOne({_id : req.body._id},user).then(
-        (result)=>{
+    User.updateOne({ _id: req.body._id }, user).then(
+        (result) => {
             if (result) {
                 console.log(result);
                 res.status(200).json({
-                    message : "USER UPDATED"
+                    message: "USER UPDATED"
                 })
             }
         }
 
     )
 })
+})
 // Business Logic :delete user
-app.delete("/users/:id",(req,res)=>{
+app.delete("/users/:id", (req, res) => {
 
     console.log("HERE IN DELETE USER");
 
     let userId = req.params.id;
-    User.deleteOne({_id : userId}).then(
-        (result)=>{
+    User.deleteOne({ _id: userId }).then(
+        (result) => {
             if (result) {
                 res.status(200).json({
-                    message : "USER DELETED"
+                    message: "USER DELETED"
                 })
             }
         }
     )
 })
 
+// Business Logic : get users by role
+app.get("/users/role/:role", (req, res) => {
+    console.log("HERE IS GET USER BY ROLE");
+    let userRole = req.params.role;
+    user.find({ role: userRole }).then(
+
+        (docs) => {
+            if (!docs) {
+                console.log("ERROR");
+            } else {
+                res.status(200).json({
+                    users: docs
+                })
+            }
+        }
+
+
+    )
+})
+
+// Business Logic : add plat
+app.post("/plats",(req,res)=>{
+    console.log("HERE IS ADD PLAT");
+    let plat = new Plat({
+        platName :req.body.platName,
+        price :req.body.price,
+        description:req.body.description,
+        idChef :req.body.idChef
+    })
+
+
+    plat.save()
+
+    res.status(200).json({
+        message: "PLAT ADDED WITH SUCCESS"
+    })
+})
+// Business Logic : get plats
+app.get("/plats/:chefId",(req,res)=>{
+    console.log("HERE IS GET PLAT BY CHEF ID");
+    let chefid = req.params.chefId;
+    Plat.findOne({chefId :chefid}).then(
+    (doc)=>{
+        if (!doc) {
+            console.log("ERROR");
+        } else {
+            res.status(200).json({
+                plat : doc
+            })
+        }
+})
+
+})
+
+// Business Logic : get all  plats
+app.get("/plats",(req,res)=>{
+    console.log("HERE IN GET ALL PLATS");
+    Plat.find((err,docs)=>{
+        if (err) {
+            console.log("ERROR");
+        } else {
+            res.status(200).json({
+                plats : docs
+            })
+        }
+    })
+})
+
+// Business Logic : update plats
+app.put("/plats/:id",(req,res)=>{
+
+    console.log("HERE IN UPDATE PLAT");
+    let plat = {
+        _id : req.body._idplatName,
+        platName : req.body.platName,
+        price : req.body.price,
+        description : req.body.description,
+        chefId : req.body.chefId,   
+    }
+
+    Plat.updateOne({_id : req.params.id},plat).then(
+
+            (result)=>{
+                if (result) {
+                    res.status(200).json({
+                        message: "PLAT UPDATED"
+                    })
+                }
+            }
+    )
+})
+
+// Business Logic : remove plats
+app.delete("/plats/:id",(req,res)=>{
+    console.log("HERE IN DELETE PLAT BY ID ");
+    let platId = req.params.id;
+    Plat.deleteOne({_id : platId}).then(
+
+        (result)=>{
+            if (result) {
+                res.status(200).json({
+                    message : "PLAT DELETED"
+                })
+            }
+        }
+    )
+})
 
 //export app
 module.exports = app;
---
+++ backend/models/plat.js
@@ -0,0 +1,18 @@
+//import Mongoose
+const mongoose = require('mongoose');
+
+//generate schema
+const platSchema = mongoose.Schema({
+
+    platName : String,
+    price : String,
+    description : String,
+    idChef : String
+
+});
+
+// generate model plat
+const plat = mongoose.model("Plat", platSchema);
+
+// export model
+module.exports = plat;
